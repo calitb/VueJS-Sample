536ffc75bcb9f7e36a782ec045ec7bc24ed8afda
Carlos Thurber <thurber.carlos@gmail.com>
2020-07-25 13:46:03 -0500

    Some Styling (#15)

--- src/pages/ListPage.vue
+++ src/pages/ListPage.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="flex flex-wrap m-4 justify-center">
     <router-link
+      tag="article"
+      class="p-3 border border-blue-200 hover:border-blue-800 cursor-pointer"
       :to="{ name: 'detail', params: { detailId: item.id } }"
       v-for="item in $store.state.items"
       :key="item.id"
     >
-      <article class="p-3 border border-blue-200 hover:border-blue-800">
-        <div class="flex justify-center items-center w-32 h-32">
-          <img :src="item.image" class="w-auto h-24" />
-        </div>
-        <div class="flex justify-center">{{ item.name }}</div>
-      </article>
+      <div class="flex justify-center items-center w-20 h-20 md:w-32 md:h-32">
+        <img :src="item.image" class="w-auto h-16 md:h-24" />
+      </div>
+      <div class="flex justify-center text-sm md:text-base w-32 text-center">
+        {{ item.name }}
+      </div>
     </router-link>
   </div>
 </template>
