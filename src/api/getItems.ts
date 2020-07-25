fb06313291de10329a4018955a809e245ffc30d9
Carlos Thurber B <thurber.carlos@gmail.com>
2020-07-25 11:31:45 -0500

    Removes Vue.filter usage

--- src/api/getItems.ts
+++ src/api/getItems.ts
@@ -1,32 +1,23 @@
-import axios, { AxiosResponse } from "axios";
-import { Item } from "@/store/state";
+import axios, { AxiosResponse } from 'axios';
 
-export type APIHandler<T> = (
-  error?: Error,
-  data?: T,
-  response?: AxiosResponse<T>
-) => void;
+import { Item } from '@/store/state';
+
+export type APIHandler<T> = (error?: Error, data?: T, response?: AxiosResponse<T>) => void;
 
 export function itemImageURL(itemName: string): string {
   const normalizedName = itemName
-    .replace("'", "")
-    .replace(".", "")
-    .replace(" ", "-")
+    .replace("'", '')
+    .replace('.', '')
+    .replace(' ', '-')
     .toLowerCase();
   return `https://img.pokemondb.net/artwork/${normalizedName}.jpg`;
 }
 
-export default async function getItems(
-  handler: APIHandler<Item[]>
-): Promise<void> {
+export default async function getItems(handler: APIHandler<Item[]>): Promise<void> {
   try {
-    const response: AxiosResponse<Item[]> = await axios.get(
-      "https://raw.githubusercontent.com/lucasbento/graphql-pokemon/master/src/pokemons/pokemons.json"
-    );
+    const response: AxiosResponse<Item[]> = await axios.get('https://raw.githubusercontent.com/lucasbento/graphql-pokemon/master/src/pokemons/pokemons.json');
     const data = response.data;
-    const items: Item[] = data.map(
-      (d): Item => ({ id: d.id, name: d.name, image: itemImageURL(d.name) })
-    );
+    const items: Item[] = data.map((d): Item => ({ id: d.id, name: d.name, image: itemImageURL(d.name) }));
     handler(undefined, items, response);
   } catch (error) {
     handler(error, undefined, undefined);
