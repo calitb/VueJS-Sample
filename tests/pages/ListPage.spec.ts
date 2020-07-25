fe44830999c7421ecb9f574c80aaa3c3e2bb2359
Carlos Thurber B <thurber.carlos@gmail.com>
2020-07-25 12:36:00 -0500

    Formatting of files is done with prettier

--- tests/pages/ListPage.spec.ts
+++ tests/pages/ListPage.spec.ts
@@ -4,7 +4,6 @@ import ListPage from "@/pages/ListPage.vue";
 import Vuex from "vuex";
 import { createLocalVue } from "@vue/test-utils";
 import itemsFixture from "../fixtures/items";
-
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
@@ -29,12 +28,12 @@ describe("List Page", () => {
     });
 
     it("should render 3 items ", () => {
-      const items = wrapper.findAll("router-link-stub");
+      const items = wrapper.findAll("article");
       expect(items).toHaveLength(3);
     });
 
     it("should render the images", () => {
-      const items = wrapper.findAll("router-link-stub");
+      const items = wrapper.findAll("article");
       const image = items.at(0).find("img");
       expect(image.attributes().src).toBe("charmander_image");
     });
